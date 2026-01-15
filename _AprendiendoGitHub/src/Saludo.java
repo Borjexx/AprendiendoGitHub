3297370d843f4fbf2658d398bc40248c0a3c8a01
Borjexx <borcabboc@alu.edu.gva.es>
2026-01-15 11:59:51 +0100

    Add files via upload

---
+++ _AprendiendoGitHub/src/Saludo.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Saludo {
+    public void saludar() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Dime tu nombre: ");
+        String nombre = scan.nextLine();
+        System.out.println("Hola " + nombre);
+    }
+}
